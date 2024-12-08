8ad94876582ec2b9b869b3d601769525ff6dec88
DainShin <di.shin0611@gmail.com>
2024-12-08 22:14:00 +0900

    added comment box

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19ebe34db629d4d27f23f29c0d13b7b38978e3a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+268bb4548af8bd68b5db02885fbed8d980f7c477")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
